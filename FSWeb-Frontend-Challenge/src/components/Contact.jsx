58fee68b5ad41ff62db6ea0f77ca8fabbd913473
Omer-Sait-Kemaloglu <omersaitkemaloglu@gmail.com>
2025-01-03 23:37:26 +0300

    dnm

--- FSWeb-Frontend-Challenge/src/components/Contact.jsx
+++ FSWeb-Frontend-Challenge/src/components/Contact.jsx
@@ -9,16 +9,16 @@ const Contact = () => {
       <p className="email">Feel free to contact me via email: omersaitkemaloglu@gmail.com</p>
       <div className="social-icons">
         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://via.placeholder.com/40" alt="Twitter" />
+          <img src="https://img.icons8.com/?size=50&id=phOKFKYpe00C&format=png" alt="Twitter" />
         </a>
         <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://via.placeholder.com/40" alt="Instagram" />
+          <img src="https://img.icons8.com/?size=48&id=Xy10Jcu1L2Su&format=png" alt="Instagram" />
         </a>
         <a href="https://github.com/Omer-Sait-Kemaloglu" target="_blank" rel="noopener noreferrer">
-          <img src="https://via.placeholder.com/40" alt="GitHub" />
+          <img src="https://img.icons8.com/?size=30&id=62856&format=png" alt="GitHub" />
         </a>
         <a href="https://www.linkedin.com/feed/?trk=nav_back_to_linkedin" target="_blank" rel="noopener noreferrer">
-          <img src="https://via.placeholder.com/40" alt="LinkedIn" />
+          <img src="https://img.icons8.com/?size=50&id=8808&format=png" alt="LinkedIn" />
         </a>
       </div>
     </section>
